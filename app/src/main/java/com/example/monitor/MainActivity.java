b20f3b04dabb8cdec2cff50acde01f655928eccf
sebastianbello <sebtrejo@outlook.es>
2023-06-13 11:03:40 -0600

    Project upload

---
+++ app/src/main/java/com/example/monitor/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.monitor;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.monitor.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        binding.eqRecycler.setLayoutManager(new LinearLayoutManager(this));
+        EqAdapter adapter = new EqAdapter();
+        adapter.setOnItemClickListener(earthquake ->
+                Toast.makeText(MainActivity.this,
+                        earthquake.getPlace(),
+                        Toast.LENGTH_SHORT).show());
+        binding.eqRecycler.setAdapter(adapter);
+        viewModel.getEqList().observe(this, eqList -> {
+            adapter.submitList(eqList);
+        });
+        viewModel.getEarthquakes();
+    }
+}
